5f1a0db617fe97548c12e07c607d97a69ae10163
judy <judy20001005@gmail.com>
2024-10-13 07:54:52 +0800

    導覽列

---
+++ js/swiper.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".swiper", {
+    effect: 'fade',
+    speed:2000,
+    fadeEffect: {
+      crossFade: true
+    },
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
