5450a6534482fceb62ee0aaef84dd4d9f094104e
judy <judy20001005@gmail.com>
2024-11-07 21:27:45 +0800

    修改首頁

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,7 @@
 var swiper1 = new Swiper(".swiper-1", {
     effect: 'fade',
     speed:2000,
+    loop: true,
     fadeEffect: {
       crossFade: true
     },
@@ -9,6 +10,10 @@ var swiper1 = new Swiper(".swiper-1", {
     autoplay: {
       delay: 4000,
       disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
 
@@ -20,23 +25,26 @@ var swiper1 = new Swiper(".swiper-1", {
       clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next1",
+      prevEl: ".swiper-button-prev1",
     },
     breakpoints: {
       // when window width is >= 480px
       480: {
         slidesPerView: 1,
-        spaceBetween: 30
+        spaceBetween: 30,
+        loop: true,
       },
       // when window width is >= 640px
       640: {
         slidesPerView: 2,
-        spaceBetween: 30
+        spaceBetween: 30,
+        loop: true,
       },
       1024: {
         slidesPerView: 3,
-        spaceBetween: 30
+        spaceBetween: 30,
+        loop: false,
       }
     }
   });
